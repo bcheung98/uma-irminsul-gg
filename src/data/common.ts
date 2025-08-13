0d4993d22370bd6c7393f52f1e3157de0faea745
Brian <briantcheung@gmail.com>
2025-08-13 16:56:24 -0700

    Add data

---
+++ src/data/common.ts
@@ -0,0 +1,2 @@
+export const rarities = [5, 4, 3, 2, 1] as const;
+export const ranks = ["A", "B", "C", "D", "E", "F", "G"] as const;
---
+++ src/data/versions.ts
@@ -0,0 +1,2 @@
+// MAKE SURE TO CHANGE THIS EVERY UPDATE!
+export const updates = [{ version: "1.0", name: "" }];
