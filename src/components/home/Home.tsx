794051bb908c488aa1bddba5463488bf6cbc3bfc
Brian <briantcheung@gmail.com>
2025-08-24 00:38:34 -0700

    Added home banner

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,14 +1,26 @@
 // Component imports
 import CurrentBanners from "components/banners/CurrentBanners";
+import { TextStyled } from "styled/StyledTypography";
 
 // MUI imports
-import { Stack } from "@mui/material";
+import { useTheme, Card, Stack } from "@mui/material";
 
 function Home() {
     document.title = `Umamusume: Pretty Derby - Irminsul.GG`;
 
+    const theme = useTheme();
+
     return (
         <Stack spacing={3}>
+            <Card sx={{ p: 2, backgroundColor: theme.background(1) }}>
+                <TextStyled>
+                    Welcome to the Umamusume: Pretty Derby branch of
+                    IRMINSUL.GG!
+                </TextStyled>
+                <TextStyled>
+                    Note that this site will be a WIP for a while.
+                </TextStyled>
+            </Card>
             <CurrentBanners />
         </Stack>
     );
