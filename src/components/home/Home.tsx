62a2dcccdf438d8bf346b9e22688829bb47cca2e
Brian <briantcheung@gmail.com>
2025-08-28 18:02:11 -0700

    Update home banner

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -13,12 +13,18 @@ function Home() {
     return (
         <Stack spacing={3}>
             <Card sx={{ p: 2, backgroundColor: theme.background(1) }}>
-                <TextStyled>
+                <TextStyled variant="h6-styled">
                     Welcome to the Umamusume: Pretty Derby branch of
                     IRMINSUL.GG!
                 </TextStyled>
+                <br />
                 <TextStyled>
-                    Note that this site will be a WIP for a while.
+                    Note that this site will be a WIP for a while, and things
+                    might be broken.
+                </TextStyled>
+                <TextStyled>
+                    If you find a bug or have any feedback, join the Discord to
+                    let me know about it!
                 </TextStyled>
             </Card>
             <CurrentBanners />
